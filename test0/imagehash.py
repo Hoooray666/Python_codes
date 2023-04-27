da8d203ce35ffadef49ed17cc34fbe09cfdd6d21
Hoooray <1569769055@qq.com>
2023-04-27 23:59:56 +0800

    1.2

--- test0/imagehash.py
+++ test0/imagehash.py
@@ -1,4 +1,6 @@
 import imagehash
 from PIL import Image
 path1 = 'E:\\Python_codes\\test0\\images\\test0.bmp'
-path2 = 'E:\\Python_codes\\test0\images\\'
+path2 = 'E:\\Python_codes\\test0\images\\football.jpg'
+
+hash1 = imagehash.average_hash(Image.open(path1))
